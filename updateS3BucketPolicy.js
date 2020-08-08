c6ec99ba79102860b20441c467e0a236f64a8d32
Ryan Housh <ryan@hou.sh>
2020-08-07 21:43:17 -0700

    Adding update S3 bucket policy lambda handler.

---
+++ updateS3BucketPolicy.js
@@ -0,0 +1,66 @@
+const S3 = require('aws-sdk/clients/s3');
+const DynamoDB = require('aws-sdk/clients/dynamodb');
+const s3 = new S3();
+const dynamodb = new DynamoDB();
+
+exports.handler = (event) => {
+    let allowIPs = [];
+
+    dynamodb.scan({ TableName: 'IPs' }, function (err, data) {
+        if (err) {
+            return {
+                statusCode: 500,
+                body: JSON.stringify(err.stack)
+            };
+        } else {
+            allowIPs = data.Items;
+
+            s3.getBucketPolicy(
+                {
+                    Bucket: process.env['bucket']
+                },
+                function (err, data) {
+                    if (err) {
+                        return {
+                            statusCode: 500,
+                            body: JSON.stringify(err.stack)
+                        };
+                    } else {
+                        let policy = JSON.parse(data.Policy);
+                        let listOfIPs = [];
+
+                        allowIPs.forEach((element) => {
+                            listOfIPs.push(element.IP.S);
+                        });
+
+                        policy.Statement[0].Condition.IpAddress[
+                            'aws:SourceIp'
+                        ] = listOfIPs;
+
+                        s3.putBucketPolicy(
+                            {
+                                Bucket: process.env['bucket'],
+                                Policy: JSON.stringify(policy)
+                            },
+                            function (err, data) {
+                                if (err) {
+                                    return {
+                                        statusCode: 500,
+                                        body: JSON.stringify(err.stack)
+                                    };
+                                } else {
+                                    return {
+                                        statusCode: 200,
+                                        body:
+                                            'Successfully updated policy for bucket, ' +
+                                            process.env['bucket']
+                                    };
+                                }
+                            }
+                        );
+                    }
+                }
+            );
+        }
+    });
+};
